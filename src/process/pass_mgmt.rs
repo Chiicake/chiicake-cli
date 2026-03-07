a352dea5cf0b0f1633ab86905ee193cb390df46b
Chiicake <pankaiwen1231@outlook.com>
2026-03-07 17:00:12 +0800

    feat: password generator

--- src/process/pass_mgmt.rs
+++ src/process/pass_mgmt.rs
@@ -1,8 +1,11 @@
 use clap::Parser;
+use rand::random;
 
 #[derive(Debug, Parser)]
 pub struct GenPassOpts {
-    ///
+    /// 密码对应的服务名称
+    #[arg(short, long, help = "Service name for the password")]
+    pub service: String,
 
     /// Length of the generated password
     #[arg(short, long, help = "Password length", default_value_t = 12)]
@@ -25,25 +28,39 @@ pub struct GenPassOpts {
     pub lc: bool,
 
     /// Include digits in the password
-    #[arg(
-        short,
-        long,
-        help = "Include digits in the password",
-        default_value_t = true
-    )]
-    pub digits: bool,
+    #[arg(long, help = "Include digits in the password", default_value_t = true)]
+    pub dg: bool,
 
     /// Include special characters in the password
     #[arg(
-        short,
         long,
         help = "Include special characters in the password",
         default_value_t = true
     )]
-    pub special: bool,
+    pub sp: bool,
 }
 
 pub fn generate_pass(opts: &GenPassOpts) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Pass to {}", opts.length);
+    let mut charset = String::new();
+    if opts.uc {
+        charset.push_str("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+    if opts.lc {
+        charset.push_str("abcdefghijklmnopqrstuvwxyz");
+    }
+    if opts.dg {
+        charset.push_str("0123456789");
+    }
+    if opts.sp {
+        charset.push_str("!@#$%^&*()-_=+[]{}|;:,.<>?/");
+    }
+
+    let mut password = String::new();
+    for _ in 0..opts.length {
+        let idx = random::<i8>() as usize % charset.len();
+        password.push(charset.chars().nth(idx).unwrap());
+    }
+
+    println!("{}", password);
     Ok(())
 }
